95a24023acc66452ce8f99bf9ac4dcb2a1478cd2
simoneceriani <simone.ceriani@gmail.com>
2020-12-30 13:14:47 +0100

    fix linux warnings

--- nelson/MatrixDenseWrapper.cpp
+++ nelson/MatrixDenseWrapper.cpp
@@ -1,4 +1,3 @@
-#pragma once
 #include "MatrixDenseWrapper.h"
 #include "MatrixDenseWrapper.hpp"
 
--- nelson/MatrixSparseWrapper.cpp
+++ nelson/MatrixSparseWrapper.cpp
@@ -1,4 +1,3 @@
-#pragma once
 #include "MatrixSparseWrapper.h"
 #include "MatrixSparseWrapper.hpp"
 
